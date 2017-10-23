2d3a252bef6cdc927aee26d3fc07dea5148e801a
sunflower2008 <donglizhog@163.com>
2017-10-23 23:37:58 +0800

    t

---
+++ CFM/wintest/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using win.CtrlExt;
+
+namespace wintest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<student> list = new List<student>();
+            list.Add(new student() { id = 1, name = "zhang" });
+            list.Add(new student() { id = 2, name = "li" });
+            this.comboBox1.DataBind(list, "id", "name", 1);
+            
+        }
+    }
+}
---
+++ CFM/wintest/sutdent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wintest
+{
+    public class student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
