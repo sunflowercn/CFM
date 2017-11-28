d502f3cdf501971c13c8a4330b12cc5f7cfb993e
sunflower2008 <donglizhog@163.com>
2017-11-28 23:14:43 +0800

    t

---
+++ CFM/win.form/Inherit/MessageBoxEx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using win.form.Properties;
+using System.Threading;
+
+
+namespace win.form.Inherit
+{
+    public enum LoadMode
+    {
+        Warning,
+        Error,
+        Prompt
+    }
+    public partial class MessageBoxEx : Form
+    {
+        /*
+        1. AW_SLIDE : 使用滑动类型, 默认为该类型. 当使用 AW_CENTER 效果时, 此效果被忽略
+        2. AW_ACTIVE: 激活窗口, 在使用了 AW_HIDE 效果时不可使用此效果
+        3. AW_BLEND: 使用淡入效果
+        4. AW_HIDE: 隐藏窗口
+        5. AW_CENTER: 与 AW_HIDE 效果配合使用则效果为窗口几内重叠,  单独使用窗口向外扩展.
+        6. AW_HOR_POSITIVE : 自左向右显示窗口
+        7. AW_HOR_NEGATIVE: 自右向左显示窗口
+        8. AW_VER_POSITVE: 自顶向下显示窗口
+        9. AW_VER_NEGATIVE : 自下向上显示窗口
+        */
+        public const Int32 AW_HOR_POSITIVE = 0x00000001;
+        public const Int32 AW_HOR_NEGATIVE = 0x00000002;
+        public const Int32 AW_VER_POSITIVE = 0x00000004;
+        public const Int32 AW_VER_NEGATIVE = 0x00000008;
+        public const Int32 AW_CENTER = 0x00000010;
+        public const Int32 AW_HIDE = 0x00010000;
+        public const Int32 AW_ACTIVATE = 0x00020000;
+        public const Int32 AW_SLIDE = 0x00040000;
+        public const Int32 AW_BLEND = 0x00080000;
+                        
+        private static LoadMode formMode = LoadMode.Prompt;
+        private static string ShowMessage = null;
+        private System.Windows.Forms.Timer Timer_Close = new System.Windows.Forms.Timer();
+
+        public MessageBoxEx()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool AnimateWindow(IntPtr hwnd, int dwTime, int dwFlags);
+
+        private void MessageBoxEx_Load(object sender, EventArgs e)
+        {
+            this.lblTitle.Text = "提示";
+            if (formMode == LoadMode.Error)
+            {
+                this.lblTitle.Text = "错误";
+                this.panelShow.BackgroundImage = Resources.error;
+            }
+            else if (formMode == LoadMode.Warning)
+            {
+                this.lblTitle.Text = "警告";
+                this.panelShow.BackgroundImage = Resources.warning;
+            }
+            else
+            {
+                this.panelShow.BackgroundImage = Resources.Prompt;
+            }
+            this.lblMessage.Text = ShowMessage;
+            int width = Screen.PrimaryScreen.Bounds.Width;
+            int num3 = (Screen.PrimaryScreen.Bounds.Height - 0x23) - base.Height;
+            int num4 = (width - base.Width) - 5;
+            base.Top = num3;
+            base.Left = num4;
+            base.TopMost = true;         
+            AnimateWindow(base.Handle, 500, 0x40008);
+            base.ShowInTaskbar = false;
+            this.Timer_Close.Interval = 2000;
+            this.Timer_Close.Tick += new EventHandler(this.Timer_Close_Tick);
+            this.Timer_Close.Start();
+        }
+
+        private void MessageBoxEx_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            AnimateWindow(base.Handle, 0x3e8, 0x50004);
+            this.Timer_Close.Stop();
+            this.Timer_Close.Dispose();
+        }
+
+        private void Timer_Close_Tick(object sender, EventArgs e)
+        {
+            this.Timer_Close.Stop();
+            base.Close();
+        }
+
+        public static void Show(LoadMode loadMode, string message)
+        {
+            formMode = loadMode;
+            ShowMessage = message;            
+            new MessageBoxEx().Show();
+        }
+
+        private void panelShow_Leave(object sender, EventArgs e)
+        {
+            this.Timer_Close.Start();
+        }
+
+        private void panelShow_Move(object sender, EventArgs e)
+        {
+            this.Timer_Close.Stop();
+        }
+    }
+}
---
+++ CFM/win.form/Inherit/MessageBoxEx.designer.cs
@@ -0,0 +1,96 @@
+﻿namespace win.form.Inherit
+{
+    partial class MessageBoxEx
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelShow = new System.Windows.Forms.Panel();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelShow.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelShow
+            // 
+            this.panelShow.BackgroundImage = global::win.form.Properties.Resources.warning;
+            this.panelShow.Controls.Add(this.lblMessage);
+            this.panelShow.Controls.Add(this.lblTitle);
+            this.panelShow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelShow.Location = new System.Drawing.Point(0, 0);
+            this.panelShow.Name = "panelShow";
+            this.panelShow.Size = new System.Drawing.Size(191, 111);
+            this.panelShow.TabIndex = 0;
+            this.panelShow.Leave += new System.EventHandler(this.panelShow_Leave);
+            this.panelShow.Move += new System.EventHandler(this.panelShow_Move);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BackColor = System.Drawing.Color.Transparent;
+            this.lblMessage.Location = new System.Drawing.Point(29, 45);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(148, 49);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "label2";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("SimSun", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTitle.Location = new System.Drawing.Point(13, 17);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(48, 15);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "警 告";
+            // 
+            // MessageBoxEx
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 111);
+            this.Controls.Add(this.panelShow);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MessageBoxEx";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "MessageBoxEx";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MessageBoxEx_FormClosed);
+            this.Load += new System.EventHandler(this.MessageBoxEx_Load);
+            this.panelShow.ResumeLayout(false);
+            this.panelShow.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelShow;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
