bf535dac89efbb5639fceb93f989b1203ba5de7b
sunflower2008 <donglizhog@163.com>
2017-12-03 01:04:49 +0800

    1

--- CFM/win.form/Properties/Resources.Designer.cs
+++ CFM/win.form/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace win.form.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap warning {
+            get {
+                object obj = ResourceManager.GetObject("warning", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
