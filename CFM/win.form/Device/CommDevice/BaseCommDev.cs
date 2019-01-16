6172fc3527c4859aee5d94d45e19102345dcec47
sunflower2018 <donglizhog@163.com>
2019-01-17 00:07:09 +0800

    q

---
+++ CFM/win.form/Device/CommDevice/BaseCommDev.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace win.form.Device.CommDevice
+{
+    public abstract class BaseCommDev
+    {
+        protected SerialPort serialport;
+        public BaseCommDev(string comprot)
+        {
+            this.serialport = new SerialPort(comprot);
+            this.serialport.DataReceived += Serialport_DataReceived;
+        }
+
+        private void Serialport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(100);
+            int count = this.serialport.BytesToRead;
+            if (count == 0)
+                return;
+
+            byte[] bytes = new byte[count];
+            this.serialport.Read(bytes, 0, count);
+        }
+
+        public void Open()
+        {
+            this.serialport.Open();
+        }
+        public void Close()
+        {
+            this.serialport.Close();
+        }
+        public void WriteData(string text)
+        {
+            if (!this.serialport.IsOpen)
+                this.Open();
+
+            this.serialport.Write(text);
+        }
+        public virtual String ReadData()
+        {
+            return null;
+        }
+    }
+}
---
+++ CFM/win.form/Device/CommDevice/Chen_SD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+namespace win.form.Device.CommDevice
+{
+    public class Chen_SD:BaseCommDev
+    {
+        public Chen_SD(string comprot) : base(comprot)
+        {
+            this.serialport.BaudRate = 9600;
+            this.serialport.StopBits = StopBits.One;
+        }
+
+        public override string ReadData()
+        {
+            return base.ReadData();
+        }
+    }
+}
