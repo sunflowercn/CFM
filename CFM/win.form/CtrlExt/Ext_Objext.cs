e207f473dc4c658967771294136cdabca2c04565
sunflower2018 <donglizhog@161.com>
2019-04-27 21:58:52 +0800

    1

---
+++ CFM/win.form/CtrlExt/Ext_Objext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace win.form.CtrlExt
+{
+    public static class Ext_Objext
+    {
+        public static  void ShowMessage(this object obj,string msg)
+        {
+            MessageBox.Show(msg);
+        }
+    }
+}
